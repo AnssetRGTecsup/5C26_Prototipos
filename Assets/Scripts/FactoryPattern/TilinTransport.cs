bd29350e9c90055f5fa309f9b46bb7b338b38eea
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-06-03 12:44:52 -0500

    tilin

---
+++ Assets/Scripts/FactoryPattern/TilinTransport.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.FactoryPattern
+{
+    public class TilinTransport : NavMeshMovement, ITransport
+    {
+        public void MoveTransport(Vector3 targetPosition)
+        {
+            agent.SetDestination(targetPosition);
+
+            Debug.Log($"Viajando a {targetPosition} por suelo");
+        }
+
+        public void SetSpawnPosition(Vector3 spawnPosition)
+        {
+            SpawnAtClosestPoint(spawnPosition);
+
+            Debug.Log($"Spawneando en {spawnPosition} modo tierra");
+        }
+    }
+}
---
+++ Assets/Scripts/FactoryPattern/TilinTransportFactory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.FactoryPattern
+{
+    public class TilinTransportFactory : TransporterAbstractFactory
+    {
+        [SerializeField] protected TilinTransport tilinTransportPrefab;
+        public override ITransport GetTransport()
+        {
+            return Instantiate(tilinTransportPrefab);
+        }
+    }
+}
